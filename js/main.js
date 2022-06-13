4b0a0d8ccca292f05aeba9f3d9ae7d0da19e3a2d
Pavel-25 <105872575+Pavel-25@users.noreply.github.com>
2022-06-13 21:20:26 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -193,3 +193,12 @@ function showStartModal() {
 		})
 	}, 12000);
 }
+
+$(document).ready(function() {
+	$('a[href^="#"]').click(function() {
+		var target = $(this).attr('href');
+		$('html, body').animate({
+			scrollTop: $(target).offset().top
+		}, 500);
+	});
+});
