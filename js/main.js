ed711276f9d14d0b82226d9fcc2abf2c6a55a6c8
Pavel-25 <105872575+Pavel-25@users.noreply.github.com>
2022-06-13 21:17:09 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -193,12 +193,3 @@ function showStartModal() {
 		})
 	}, 12000);
 }
-
-$(document).ready(function() {
-	$('a[href^="#"]').click(function() {
-		var target = $(this).attr('href');
-		$('html, body').animate({
-			scrollTop: $(target).offset().top
-		}, 500);
-	});
-});
