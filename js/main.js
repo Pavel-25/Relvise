4cc92399cac6e7b7347f54efdda23f480d0a1409
Pavel-25 <105872575+Pavel-25@users.noreply.github.com>
2022-06-13 23:09:12 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -201,4 +201,10 @@ $(document).ready(function() {
 			scrollTop: $(target).offset().top
 		}, 500);
 	});
+	$('a[data-goto^=""]').click(function() {
+		var target = $(this).attr('data-goto');
+		$('html, body').animate({
+			scrollTop: $(target).offset().top
+		}, 500);
+	});
 });
